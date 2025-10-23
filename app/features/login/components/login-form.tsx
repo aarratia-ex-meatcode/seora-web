e7d532722bea679caeff0e04ec0bcabf87925bba
bruno.faoro <bfaoro.ex@meatcode.cl>
2025-10-23 09:51:00 -0400

    refactor(login): improve layout

--- app/features/login/components/login-form.tsx
+++ app/features/login/components/login-form.tsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Field, FieldDescription, FieldGroup, FieldLabel } from "@/components/ui/field";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
 import { Form } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useLogin } from "@/features/login/hooks/use-login";
@@ -28,78 +32,76 @@ export function LoginForm() {
   const { formState } = form;
   const loginMutation = useLogin();
 
-  const onSubmit = async (data: any) => {
-    loginMutation.mutate({
-      email: data.email,
-      password: data.password,
-    });
+  const onSubmit = async ({ email, password }: z.infer<typeof formSchema>) => {
+    loginMutation.mutate({ email, password });
   };
 
   return (
-    <div className="flex flex-col gap-6">
-      <Card className="shadow-none">
-        <CardHeader>
-          <CardTitle className="text-[#0e1111]">
-            {messages.login.title}
-          </CardTitle>
-          <CardDescription className="text-[#414a4c]">
+    <div className="rounded-xl border p-6 backdrop-blur-sm md:p-9">
+      <div className="flex flex-col gap-y-8">
+        <div className="space-y-1.5">
+          <h1 className="text-xl font-medium">{messages.login.title}</h1>
+          <p className="text-muted-foreground text-sm">
             {messages.login.description}
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-              <FieldGroup>
-                <Field>
-                  <FieldLabel htmlFor="email">{messages.login.form.email.label}</FieldLabel>
-                  <Input
-                    shadow="none"
-                    dimensions="xl"
-                    id="email"
-                    type="email"
-                    {...form.register("email")}
-                  />
-                </Field>
-                <Field>
-                  <div className="flex items-center">
-                    <FieldLabel htmlFor="password">{messages.login.form.password.label}</FieldLabel>
-                    <a
-                      href="#"
-                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                    >
-                      {messages.login.form.password.forgot}
-                    </a>
-                  </div>
-                  <Input
-                    shadow="none"
-                    dimensions="xl"
-                    id="password"
-                    type="password"
-                    {...form.register("password")}
-                  />
-                </Field>
-                <Field>
-                  <Button
-                    type="submit"
-                    variant="soft"
-                    size="xl"
-                    className="w-full font-medium"
-                    disabled={formState.isSubmitting || loginMutation.isPending}
+          </p>
+        </div>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">
+                  {messages.login.form.email.label}
+                </FieldLabel>
+                <Input
+                  shadow="none"
+                  dimensions="xl"
+                  id="email"
+                  type="email"
+                  {...form.register("email")}
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">
+                    {messages.login.form.password.label}
+                  </FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                   >
-                    {loginMutation.isPending 
-                      ? messages.login.form.submit.loggingIn 
-                      : messages.login.form.submit.login}
-                  </Button>
-                  <FieldDescription className="text-center">
-                    {messages.login.form.signup.text}{" "}
-                    <a href="#">{messages.login.form.signup.link}</a>
-                  </FieldDescription>
-                </Field>
-              </FieldGroup>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
+                    {messages.login.form.password.forgot}
+                  </a>
+                </div>
+                <Input
+                  shadow="none"
+                  dimensions="xl"
+                  id="password"
+                  type="password"
+                  {...form.register("password")}
+                />
+              </Field>
+              <Field className="gap-y-4">
+                <Button
+                  type="submit"
+                  variant="soft"
+                  size="xl"
+                  className="w-full font-medium"
+                  disabled={formState.isSubmitting || loginMutation.isPending}
+                >
+                  {loginMutation.isPending
+                    ? messages.login.form.submit.loggingIn
+                    : messages.login.form.submit.login}
+                </Button>
+                <FieldDescription className="text-center">
+                  {messages.login.form.signup.text}{" "}
+                  <a href="#">{messages.login.form.signup.link}</a>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 }
