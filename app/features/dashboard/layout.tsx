7864edeb7949aef43c0573b1596153234e8e318f
bruno.faoro <bfaoro.ex@meatcode.cl>
2025-10-24 14:51:43 -0400

    refactor(dashboard): clean up layout by commenting out unused code

--- app/features/dashboard/layout.tsx
+++ app/features/dashboard/layout.tsx
@@ -28,49 +28,50 @@ export default function DashboardLayout() {
 
   return (
     <>
-      {/*
       <SidebarProvider style={SIDEBAR_PROVIDER_STYLES}>
-        <AppSidebar variant="sidebar" />
+        {/* <AppSidebar variant="sidebar" /> */}
         <SidebarInset className="bg-background">
           <SiteHeader />
-          <div className="flex flex-1 flex-col">
-            <div className="@container/main flex flex-1 flex-col gap-2">
-              <div className="flex flex-col gap-4 p-6 lg:p-8">
-                <Outlet context={{ user, token }} />
-              </div>
-            </div>
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
-      */}
 
-      <Block className="flex min-h-svh flex-col">
-        <Block className="flex w-full flex-1 flex-col">
-          <Block className="sticky top-0 z-10">
-            <Header className="px-4 sm:px-8">
-              <Nav className="flex h-16 flex-row items-center justify-between">
-                <Block>
-                  <Logo />
+          <Block className="flex min-h-svh flex-col">
+            <Block className="flex w-full flex-1 flex-col">
+              <Block className="sticky top-0 z-10">
+                <Header className="px-4 sm:px-8">
+                  <Nav className="flex h-16 flex-row items-center justify-between">
+                    <Block>
+                      <Logo />
+                    </Block>
+                    <Block>
+                      <ProfileDropdown />
+                    </Block>
+                  </Nav>
+                </Header>
+                <Block className="px-4 sm:px-8">
+                  <NavTabs />
                 </Block>
-                <Block>
-                  <ProfileDropdown />
+              </Block>
+
+              <Main className="flex flex-1 flex-col px-2 pt-px pb-2">
+                <Block className="ring-background @container/main flex grow flex-col rounded-md ring-1">
+                  <Block className="px-4 py-6 lg:px-6 lg:py-8">
+                    <Outlet context={{ user, token }} />
+                  </Block>
                 </Block>
-              </Nav>
-            </Header>
-            <Block className="px-4 sm:px-8">
-              <NavTabs />
+              </Main>
             </Block>
           </Block>
 
-          <Main className="flex flex-1 flex-col px-2 pt-px pb-2">
-            <Block className="ring-background @container/main flex grow flex-col rounded-md ring-1">
-              <Block className="px-4 py-6 lg:px-6 lg:py-8">
-                <Outlet context={{ user, token }} />
-              </Block>
-            </Block>
-          </Main>
-        </Block>
-      </Block>
+          {/*
+            <div className="flex flex-1 flex-col">
+              <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 p-6 lg:p-8">
+                  <Outlet context={{ user, token }} />
+                </div>
+              </div>
+            </div>
+          */}
+        </SidebarInset>
+      </SidebarProvider>
     </>
   );
 }
