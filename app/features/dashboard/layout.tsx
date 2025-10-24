877ba0d0e11b5cf2e14f6bcca18f35cf99a54a01
bruno.faoro <bfaoro.ex@meatcode.cl>
2025-10-24 14:14:51 -0400

    refactor(dashboard): restructure layout to incorporate Main component

--- app/features/dashboard/layout.tsx
+++ app/features/dashboard/layout.tsx
@@ -3,6 +3,7 @@
 import { Logo } from "@/components/common/logo";
 import { Block } from "@/components/primitives/block";
 import { Header } from "@/components/primitives/header";
+import { Main } from "@/components/primitives/main";
 import { Nav } from "@/components/primitives/nav";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/features/dashboard/components/app-sidebar";
@@ -27,9 +28,24 @@ export default function DashboardLayout() {
 
   return (
     <>
+      {/*
       <SidebarProvider style={SIDEBAR_PROVIDER_STYLES}>
         <AppSidebar variant="sidebar" />
         <SidebarInset className="bg-background">
+          <SiteHeader />
+          <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+              <div className="flex flex-col gap-4 p-6 lg:p-8">
+                <Outlet context={{ user, token }} />
+              </div>
+            </div>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+      */}
+
+      <Block className="flex min-h-svh flex-col">
+        <Block className="flex w-full flex-1 flex-col">
           <Block className="sticky top-0 z-10">
             <Header className="px-4 sm:px-8">
               <Nav className="flex h-16 flex-row items-center justify-between">
@@ -41,23 +57,22 @@ export default function DashboardLayout() {
                 </Block>
               </Nav>
             </Header>
-
             <Block className="px-4 sm:px-8">
               <NavTabs />
             </Block>
           </Block>
 
-          <SiteHeader />
-
-          <div className="flex flex-1 flex-col">
-            <div className="@container/main flex flex-1 flex-col gap-2">
-              <div className="flex flex-col gap-4 p-6 lg:p-8">
-                <Outlet context={{ user, token }} />
-              </div>
-            </div>
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
+          <Main className="flex flex-1 flex-col px-2 pt-px pb-2">
+            <Block className="ring-background @container/main flex grow flex-col rounded-md ring-1">
+              <Block className="px-4 py-6 lg:px-6 lg:py-8">
+                <Block className="flex flex-col gap-y-4">
+                  <Outlet context={{ user, token }} />
+                </Block>
+              </Block>
+            </Block>
+          </Main>
+        </Block>
+      </Block>
     </>
   );
 }
